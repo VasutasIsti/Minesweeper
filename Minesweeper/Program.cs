11ce2cb63d38ee1d29465533b4a39faf8eff3783
VasutasIsti <isti.nagy03@gmail.com>
2023-08-01 21:40:29 +0200

    Removed unused libraries

--- Minesweeper/Program.cs
+++ Minesweeper/Program.cs
@@ -4,10 +4,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Net;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Minesweeper
